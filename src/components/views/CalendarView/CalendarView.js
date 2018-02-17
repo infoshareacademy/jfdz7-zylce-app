adb9f6ad244641057338c891a25612a032ead484
Lukasz <lukaszsztormowski@gmail.com>
2018-02-17 14:03:54 +0100

    add CalerndarView

---
+++ src/components/views/CalendarView/CalendarView.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+class CalendarView extends Component {
+    render() {
+        return (
+            <React.Fragment>
+                <p>test test</p>
+            </React.Fragment>
+        )
+    }
+}
+
+export default CalendarView
