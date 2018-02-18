25e57e3e7f5b8d6735148774ad81ff3e92d57d5d
moni <monika.rusajczyk@gmail.com>
2018-02-18 12:13:36 +0100

    create basic big-calendar

---
+++ src/components/views/CalendarView/Calendar.js
@@ -0,0 +1,31 @@
+import React from 'react'
+import BigCalendar from 'react-big-calendar'
+import moment from 'moment'
+import 'moment/locale/nb';
+import 'react-big-calendar/lib/css/react-big-calendar.css'
+import events from './dataForTest/events'
+
+BigCalendar.momentLocalizer(moment);
+
+class Calendar extends React.Component {
+    constructor() {
+        super();
+        this.state = {
+            events: [],
+        };
+    }
+
+    render() {
+        return (
+            <React.Fragment>
+                <h1>kalendarz</h1>
+                <BigCalendar
+                    defaultDate={new Date(2015, 3, 1)}
+                    events={events}
+                />
+            </React.Fragment>
+        )
+    }
+};
+
+export default Calendar
--- src/components/views/CalendarView/CalendarView.js
+++ src/components/views/CalendarView/CalendarView.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react'
+import Calendar from './Calendar'
 
 class CalendarView extends Component {
     render() {
         return (
             <React.Fragment>
                 <p>test test</p>
+                <Calendar />
             </React.Fragment>
         )
     }
