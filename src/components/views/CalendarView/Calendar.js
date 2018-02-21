c5ab50c59d9fce3b5252e10eb5c52642a088220a
moni <monika.rusajczyk@gmail.com>
2018-02-21 18:08:06 +0100

    hide <allDay> row from week and day view

--- src/components/views/CalendarView/Calendar.js
+++ src/components/views/CalendarView/Calendar.js
@@ -9,6 +9,17 @@ import events from './dataForTest/events'
 moment.locale('pl');
 BigCalendar.setLocalizer(BigCalendar.momentLocalizer(moment));
 
+let formats = {
+    dayFormat: (date, culture, localizer) =>
+        localizer.format(date, 'dd, DoMM', culture),
+
+    dayRangeHeaderFormat: ({start, end}, culture, localizer) =>
+        localizer.format(start, 'DoMM', culture) + ' - ' + localizer.format(end, 'DoMM', culture),
+
+    dayHeaderFormat: (date, culture, localizer) =>
+        localizer.format(date, 'dddd, DD MMMM', culture)
+};
+
 class Calendar extends React.Component {
     state = {
         messages: {
@@ -27,7 +38,6 @@ class Calendar extends React.Component {
 
     };
 
-
     render() {
 
         return (
@@ -45,6 +55,7 @@ class Calendar extends React.Component {
                     popup
                     timeslots={4}
                     defaultDate={new Date()}
+                    formats={formats}
                 />
                 </div>
             </React.Fragment>
